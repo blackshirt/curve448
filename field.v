1d03a00f22b134e1698c5cb2fbe63664bef023f4
blackshirt <blackshirtmuslim@yahoo.com>
2026-07-24 15:16:16 +0700

    remove reduce on is_canonical()

--- field.v
+++ field.v
@@ -645,20 +645,12 @@ fn (mut z Field) set_bytes(b []u8) ! {
 }
 
 // Check if field element is in canonical form [0, p-1]
-fn (x Field) is_canonical() bool {
+fn (z Field) is_canonical() bool {
 	// We directly checks individual limbs with z.el[i] > mask_56bits.
 	// However, if a field element has unpropagated carries (e.g., el[0] = 2^56
-	// while the total value is still modulo $< p$),
+	// while the total value is still modulo < p,
 	// it will incorrectly return false, so we reduce it firts
-	mut z := x
-
-	// when we use fe_reduce() performs full modular reduction —
-	// by definition its output is always in [0, p-1].
-	// The function then checks whether that already-reduced value is >= p.
-	// That can never be true. is_canonical() always returns true, for every input, no matter what.
-	// Return false if any limb exceeds 56 bits. so, we use fe_weak_reduce() instead.
-	fe_weak_reduce(mut z) // normalize limb form only — do NOT reduce mod p
-
+	// mut z := x
 	for i := 0; i < 8; i++ {
 		if z.el[i] > mask_56bits { return false }
 	}
